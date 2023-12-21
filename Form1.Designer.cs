59ac2d374510b06f284203f2ea1f70975cc59eb1
Leeevai <hassanmohseen22@gmail.com>
2023-12-21 10:54:42 +0200

    login page

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -1,5 +1,5 @@
 ﻿namespace OnlyFriends {
-	partial class Form1 {
+	partial class Login {
 		/// <summary>
 		/// Required designer variable.
 		/// </summary>
@@ -23,90 +23,159 @@
 		/// the contents of this method with the code editor.
 		/// </summary>
 		private void InitializeComponent() {
-			this.submitButton = new System.Windows.Forms.Button();
-			this.emailBox = new System.Windows.Forms.TextBox();
-			this.passwordBox = new System.Windows.Forms.TextBox();
-			this.titleBox = new System.Windows.Forms.TextBox();
-			this.contentBox = new System.Windows.Forms.TextBox();
-			this.button1 = new System.Windows.Forms.Button();
-			this.SuspendLayout();
-			// 
-			// submitButton
-			// 
-			this.submitButton.Location = new System.Drawing.Point(92, 213);
-			this.submitButton.Name = "submitButton";
-			this.submitButton.Size = new System.Drawing.Size(159, 74);
-			this.submitButton.TabIndex = 0;
-			this.submitButton.Text = "Click Me";
-			this.submitButton.UseVisualStyleBackColor = true;
-			this.submitButton.Click += new System.EventHandler(this.button1_Click);
-			// 
-			// emailBox
-			// 
-			this.emailBox.Location = new System.Drawing.Point(44, 86);
-			this.emailBox.Name = "emailBox";
-			this.emailBox.Size = new System.Drawing.Size(237, 22);
-			this.emailBox.TabIndex = 1;
-			this.emailBox.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
-			// 
-			// passwordBox
-			// 
-			this.passwordBox.Location = new System.Drawing.Point(44, 140);
-			this.passwordBox.Name = "passwordBox";
-			this.passwordBox.Size = new System.Drawing.Size(237, 22);
-			this.passwordBox.TabIndex = 2;
-			this.passwordBox.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
-			// 
-			// titleBox
-			// 
-			this.titleBox.Location = new System.Drawing.Point(474, 86);
-			this.titleBox.Name = "titleBox";
-			this.titleBox.Size = new System.Drawing.Size(218, 22);
-			this.titleBox.TabIndex = 3;
-			// 
-			// contentBox
-			// 
-			this.contentBox.Location = new System.Drawing.Point(474, 172);
-			this.contentBox.Name = "contentBox";
-			this.contentBox.Size = new System.Drawing.Size(218, 22);
-			this.contentBox.TabIndex = 4;
-			// 
-			// button1
-			// 
-			this.button1.Location = new System.Drawing.Point(519, 229);
-			this.button1.Name = "button1";
-			this.button1.Size = new System.Drawing.Size(113, 58);
-			this.button1.TabIndex = 5;
-			this.button1.Text = "Post";
-			this.button1.UseVisualStyleBackColor = true;
-			this.button1.Click += new System.EventHandler(this.button1_Click_1);
-			// 
-			// Form1
-			// 
-			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
-			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(800, 450);
-			this.Controls.Add(this.button1);
-			this.Controls.Add(this.contentBox);
-			this.Controls.Add(this.titleBox);
-			this.Controls.Add(this.passwordBox);
-			this.Controls.Add(this.emailBox);
-			this.Controls.Add(this.submitButton);
-			this.Name = "Form1";
-			this.Text = "Form1";
-			this.ResumeLayout(false);
-			this.PerformLayout();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Login));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.loginBTN = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.invalidEmaillbl = new System.Windows.Forms.Label();
+            this.InvalidPasswordlbl = new System.Windows.Forms.Label();
+            this.form2btn = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.BackgroundImage = global::OnlyFriends.Properties.Resources.onlyfans_logo_152221;
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pictureBox1.Location = new System.Drawing.Point(268, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(208, 166);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(218, 260);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(302, 20);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Text = "Username/Email";
+            this.textBox1.Leave += new System.EventHandler(this.textBox1_Leave);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(218, 305);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(302, 20);
+            this.textBox2.TabIndex = 2;
+            this.textBox2.Text = "Password";
+            this.textBox2.Enter += new System.EventHandler(this.textBox2_Enter);
+            // 
+            // loginBTN
+            // 
+            this.loginBTN.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.loginBTN.Location = new System.Drawing.Point(268, 352);
+            this.loginBTN.Name = "loginBTN";
+            this.loginBTN.Size = new System.Drawing.Size(91, 36);
+            this.loginBTN.TabIndex = 3;
+            this.loginBTN.Text = "Login";
+            this.loginBTN.UseVisualStyleBackColor = true;
+            this.loginBTN.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.button2.Location = new System.Drawing.Point(385, 352);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(91, 36);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Sign-up";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // invalidEmaillbl
+            // 
+            this.invalidEmaillbl.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.invalidEmaillbl.AutoSize = true;
+            this.invalidEmaillbl.BackColor = System.Drawing.Color.Transparent;
+            this.invalidEmaillbl.ForeColor = System.Drawing.SystemColors.MenuBar;
+            this.invalidEmaillbl.Location = new System.Drawing.Point(215, 244);
+            this.invalidEmaillbl.Name = "invalidEmaillbl";
+            this.invalidEmaillbl.Size = new System.Drawing.Size(68, 13);
+            this.invalidEmaillbl.TabIndex = 6;
+            this.invalidEmaillbl.Text = "*invalid email";
+            this.invalidEmaillbl.Visible = false;
+            // 
+            // InvalidPasswordlbl
+            // 
+            this.InvalidPasswordlbl.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.InvalidPasswordlbl.AutoSize = true;
+            this.InvalidPasswordlbl.BackColor = System.Drawing.Color.Transparent;
+            this.InvalidPasswordlbl.ForeColor = System.Drawing.SystemColors.MenuBar;
+            this.InvalidPasswordlbl.Location = new System.Drawing.Point(215, 289);
+            this.InvalidPasswordlbl.Name = "InvalidPasswordlbl";
+            this.InvalidPasswordlbl.Size = new System.Drawing.Size(89, 13);
+            this.InvalidPasswordlbl.TabIndex = 7;
+            this.InvalidPasswordlbl.Text = "*invalid password";
+            this.InvalidPasswordlbl.Visible = false;
+            // 
+            // form2btn
+            // 
+            this.form2btn.Location = new System.Drawing.Point(557, 351);
+            this.form2btn.Name = "form2btn";
+            this.form2btn.Size = new System.Drawing.Size(86, 36);
+            this.form2btn.TabIndex = 8;
+            this.form2btn.Text = "form 2";
+            this.form2btn.UseVisualStyleBackColor = true;
+            this.form2btn.Click += new System.EventHandler(this.form2btn_Click);
+            // 
+            // Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.BackgroundImage = global::OnlyFriends.Properties.Resources.ripples_in_blank_black_6m97l43pauyhiw4e1;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(719, 429);
+            this.Controls.Add(this.form2btn);
+            this.Controls.Add(this.InvalidPasswordlbl);
+            this.Controls.Add(this.invalidEmaillbl);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.loginBTN);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.MinimumSize = new System.Drawing.Size(735, 449);
+            this.Name = "Login";
+            this.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.Text = "Login";
+            this.TopMost = true;
+            this.TransparencyKey = System.Drawing.Color.RosyBrown;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
 
 		}
 
-		#endregion
+        #endregion
 
-		private System.Windows.Forms.Button submitButton;
-		private System.Windows.Forms.TextBox emailBox;
-		private System.Windows.Forms.TextBox passwordBox;
-		private System.Windows.Forms.TextBox titleBox;
-		private System.Windows.Forms.TextBox contentBox;
-		private System.Windows.Forms.Button button1;
-	}
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button loginBTN;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label invalidEmaillbl;
+        private System.Windows.Forms.Label InvalidPasswordlbl;
+        private System.Windows.Forms.Button form2btn;
+    }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -3,10 +3,11 @@ using System.Collections.Generic;
 using System.Windows.Forms;
 
 namespace OnlyFriends {
-	public partial class Form1 : Form {
-		public Form1() {
+	public partial class Login : Form {
+		public Login() {
 			InitializeComponent();
 		}
+		/*
 		private void button1_Click(object sender, EventArgs e) {
 
 			DatabaseConnection connection = DatabaseConnection.Instance;
@@ -48,6 +49,41 @@ namespace OnlyFriends {
 		private void textBox2_TextChanged(object sender, EventArgs e) {
 
 		}
+		*/
+        private void Form1_Load(object sender, EventArgs e) {
 
-	}
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e) {
+
+        }
+		private bool flag=true;
+        private void textBox2_Enter(object sender, EventArgs e) {
+            if(flag){textBox2.Text = string.Empty;flag=false;}
+            textBox2.UseSystemPasswordChar = true;
+        }
+
+		private void textBox1_Leave(object sender, EventArgs e) {
+			//check valid characters for username
+			//check for cotrect email format
+			//query request
+			//change the lbl visibility in case for any error
+			
+		}
+		//to press any buttons there mustn't be any errors and thus the labels must be noot visble
+        private void button1_Click(object sender, EventArgs e) {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e) {
+
+        }
+
+        private void form2btn_Click(object sender, EventArgs e) {
+			Form2 form2=new Form2();
+			this.Hide();
+			form2.Show();
+
+        }
+    }
 }
