dca452387a87e9969af1f9faaf1552bcd48bc17e
Leeevai <hassanmohseen22@gmail.com>
2023-12-23 16:01:19 +0200

    scrolling features!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!

--- Post.cs
+++ Post.cs
@@ -1,18 +1,23 @@
 ﻿using MySqlConnector;
 using System;
 using System.Collections.Generic;
+using System.Drawing;
 
 namespace OnlyFriends {
 	internal class Post {
-
+		public Image Pic { get; set; }
 		public int PostId { get; set; }
 		public int UserId { get; set; }
 		public string Title { get; set; }
 		public string Content { get; set; }
 		public DateTime TimeStamp { get; set; }
-		public int Likes { get; set; }
+		public int Likes { get; set; } 
+		public int Comments{get;set;}
+		public int Shares { get; set; }
+		public List<string> tags { get; set; }
 		public Post(int postId, int userId, string title, string content, DateTime timeStamp, int likes) {
-
+			tags = new List<string>();
+			tags.Add("NewPost");
 			PostId = postId;
 			UserId = userId;
 			Title = title;
