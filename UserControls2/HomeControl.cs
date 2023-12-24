6b4291492275583e7981a8d6cbb46cba50cc6220
AliAbdallah454 <alikhaledabdallah454@gmail.com>
2023-12-24 20:27:36 +0200

    Made the UI

--- UserControls2/HomeControl.cs
+++ UserControls2/HomeControl.cs
@@ -1,17 +1,31 @@
-﻿using System;
+﻿using OnlyFriends.Components;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace OnlyFriends.UserControls2 {
-    public partial class HomeControl : UserControl {
-        public HomeControl() {
-            InitializeComponent();
-        }
-    }
+	public partial class HomeControl : UserControl {
+		public HomeControl() {
+			InitializeComponent();
+			populate();
+		}
+
+		public void populate() {
+
+			User user = User.Instance;
+
+			List<Post> posts = user.getFeedPosts();
+			int nbOfPosts = posts.Count;
+
+			Tweet[] tweets = new Tweet[nbOfPosts];
+
+			for (int i = 0; i < posts.Count; i++) {
+				tweets[i] = new Tweet();
+				tweets[i].UName = HelperFunctions.translateUserIdToUserName(posts[i].UserId);
+				flowLayoutPanel1.Controls.Add(tweets[i]);
+			}
+
+
+		}
+
+	}
 }
