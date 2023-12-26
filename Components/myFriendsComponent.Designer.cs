983bc3e43c03b831c27e30ac605f36a0caea22f4
AliAbdallah454 <alikhaledabdallah454@gmail.com>
2023-12-26 20:02:33 +0200

    Done

--- Components/myFriendsComponent.Designer.cs
+++ Components/myFriendsComponent.Designer.cs
@@ -23,101 +23,108 @@
         /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent() {
-            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
-            this.usernameLabel = new System.Windows.Forms.Button();
-            this.emailLabel = new System.Windows.Forms.Label();
-            this.removeFriendButton = new System.Windows.Forms.Button();
-            this.userPicture = new System.Windows.Forms.PictureBox();
-            this.tableLayoutPanel1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.userPicture)).BeginInit();
-            this.SuspendLayout();
-            // 
-            // tableLayoutPanel1
-            // 
-            this.tableLayoutPanel1.ColumnCount = 2;
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 35F));
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 165F));
-            this.tableLayoutPanel1.Controls.Add(this.userPicture, 0, 0);
-            this.tableLayoutPanel1.Controls.Add(this.usernameLabel, 1, 0);
-            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Top;
-            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
-            this.tableLayoutPanel1.Margin = new System.Windows.Forms.Padding(9, 30, 9, 9);
-            this.tableLayoutPanel1.MaximumSize = new System.Drawing.Size(200, 35);
-            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
-            this.tableLayoutPanel1.RowCount = 1;
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 35F));
-            this.tableLayoutPanel1.Size = new System.Drawing.Size(200, 35);
-            this.tableLayoutPanel1.TabIndex = 1;
-            // 
-            // usernameLabel
-            // 
-            this.usernameLabel.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.usernameLabel.FlatAppearance.BorderSize = 0;
-            this.usernameLabel.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.usernameLabel.Font = new System.Drawing.Font("Arial Rounded MT Bold", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.usernameLabel.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(126)))), ((int)(((byte)(249)))));
-            this.usernameLabel.Location = new System.Drawing.Point(38, 3);
-            this.usernameLabel.Name = "usernameLabel";
-            this.usernameLabel.Size = new System.Drawing.Size(159, 29);
-            this.usernameLabel.TabIndex = 1;
-            this.usernameLabel.Text = "Username";
-            this.usernameLabel.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.usernameLabel.UseVisualStyleBackColor = true;
-            // 
-            // emailLabel
-            // 
-            this.emailLabel.AutoSize = true;
-            this.emailLabel.Font = new System.Drawing.Font("Arial Rounded MT Bold", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.emailLabel.Location = new System.Drawing.Point(42, 35);
-            this.emailLabel.Name = "emailLabel";
-            this.emailLabel.Size = new System.Drawing.Size(44, 15);
-            this.emailLabel.TabIndex = 2;
-            this.emailLabel.Text = "Email";
-            // 
-            // removeFriendButton
-            // 
-            this.removeFriendButton.BackColor = System.Drawing.Color.Gainsboro;
-            this.removeFriendButton.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.removeFriendButton.FlatAppearance.BorderSize = 0;
-            this.removeFriendButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.removeFriendButton.Font = new System.Drawing.Font("Arial Rounded MT Bold", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.removeFriendButton.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(253)))), ((int)(((byte)(60)))), ((int)(((byte)(79)))));
-            this.removeFriendButton.Image = global::OnlyFriends.Properties.Resources.icons8_remove_30;
-            this.removeFriendButton.Location = new System.Drawing.Point(507, 23);
-            this.removeFriendButton.Name = "removeFriendButton";
-            this.removeFriendButton.Size = new System.Drawing.Size(130, 45);
-            this.removeFriendButton.TabIndex = 3;
-            this.removeFriendButton.Text = "Remove";
-            this.removeFriendButton.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.removeFriendButton.TextImageRelation = System.Windows.Forms.TextImageRelation.TextBeforeImage;
-            this.removeFriendButton.UseVisualStyleBackColor = false;
-            // 
-            // userPicture
-            // 
-            this.userPicture.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.userPicture.Image = global::OnlyFriends.Properties.Resources.icons8_male_user_35;
-            this.userPicture.Location = new System.Drawing.Point(3, 3);
-            this.userPicture.Name = "userPicture";
-            this.userPicture.Size = new System.Drawing.Size(29, 29);
-            this.userPicture.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
-            this.userPicture.TabIndex = 0;
-            this.userPicture.TabStop = false;
-            // 
-            // MyFriendsComponent
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Controls.Add(this.removeFriendButton);
-            this.Controls.Add(this.emailLabel);
-            this.Controls.Add(this.tableLayoutPanel1);
-            this.MaximumSize = new System.Drawing.Size(690, 90);
-            this.MinimumSize = new System.Drawing.Size(690, 90);
-            this.Name = "MyFriendsComponent";
-            this.Size = new System.Drawing.Size(690, 90);
-            this.tableLayoutPanel1.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)(this.userPicture)).EndInit();
-            this.ResumeLayout(false);
-            this.PerformLayout();
+			this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+			this.userPicture = new System.Windows.Forms.PictureBox();
+			this.usernameLabel = new System.Windows.Forms.Button();
+			this.emailLabel = new System.Windows.Forms.Label();
+			this.removeFriendButton = new System.Windows.Forms.Button();
+			this.tableLayoutPanel1.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)(this.userPicture)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// tableLayoutPanel1
+			// 
+			this.tableLayoutPanel1.ColumnCount = 2;
+			this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 47F));
+			this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 220F));
+			this.tableLayoutPanel1.Controls.Add(this.userPicture, 0, 0);
+			this.tableLayoutPanel1.Controls.Add(this.usernameLabel, 1, 0);
+			this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Top;
+			this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+			this.tableLayoutPanel1.Margin = new System.Windows.Forms.Padding(12, 37, 12, 11);
+			this.tableLayoutPanel1.MaximumSize = new System.Drawing.Size(267, 43);
+			this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+			this.tableLayoutPanel1.RowCount = 1;
+			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 43F));
+			this.tableLayoutPanel1.Size = new System.Drawing.Size(267, 43);
+			this.tableLayoutPanel1.TabIndex = 1;
+			// 
+			// userPicture
+			// 
+			this.userPicture.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.userPicture.Image = global::OnlyFriends.Properties.Resources.icons8_male_user_35;
+			this.userPicture.Location = new System.Drawing.Point(4, 4);
+			this.userPicture.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+			this.userPicture.Name = "userPicture";
+			this.userPicture.Size = new System.Drawing.Size(39, 35);
+			this.userPicture.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+			this.userPicture.TabIndex = 0;
+			this.userPicture.TabStop = false;
+			// 
+			// usernameLabel
+			// 
+			this.usernameLabel.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.usernameLabel.FlatAppearance.BorderSize = 0;
+			this.usernameLabel.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.usernameLabel.Font = new System.Drawing.Font("Arial Rounded MT Bold", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.usernameLabel.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(126)))), ((int)(((byte)(249)))));
+			this.usernameLabel.Location = new System.Drawing.Point(51, 4);
+			this.usernameLabel.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+			this.usernameLabel.Name = "usernameLabel";
+			this.usernameLabel.Size = new System.Drawing.Size(212, 35);
+			this.usernameLabel.TabIndex = 1;
+			this.usernameLabel.Text = "Username";
+			this.usernameLabel.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+			this.usernameLabel.UseVisualStyleBackColor = true;
+			// 
+			// emailLabel
+			// 
+			this.emailLabel.AutoSize = true;
+			this.emailLabel.Font = new System.Drawing.Font("Arial Rounded MT Bold", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.emailLabel.Location = new System.Drawing.Point(56, 43);
+			this.emailLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+			this.emailLabel.Name = "emailLabel";
+			this.emailLabel.Size = new System.Drawing.Size(55, 20);
+			this.emailLabel.TabIndex = 2;
+			this.emailLabel.Text = "Email";
+			// 
+			// removeFriendButton
+			// 
+			this.removeFriendButton.BackColor = System.Drawing.Color.Gainsboro;
+			this.removeFriendButton.Cursor = System.Windows.Forms.Cursors.Hand;
+			this.removeFriendButton.FlatAppearance.BorderSize = 0;
+			this.removeFriendButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.removeFriendButton.Font = new System.Drawing.Font("Arial Rounded MT Bold", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.removeFriendButton.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(253)))), ((int)(((byte)(60)))), ((int)(((byte)(79)))));
+			this.removeFriendButton.Image = global::OnlyFriends.Properties.Resources.icons8_remove_30;
+			this.removeFriendButton.Location = new System.Drawing.Point(676, 28);
+			this.removeFriendButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+			this.removeFriendButton.Name = "removeFriendButton";
+			this.removeFriendButton.Size = new System.Drawing.Size(173, 55);
+			this.removeFriendButton.TabIndex = 3;
+			this.removeFriendButton.Text = "Remove";
+			this.removeFriendButton.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+			this.removeFriendButton.TextImageRelation = System.Windows.Forms.TextImageRelation.TextBeforeImage;
+			this.removeFriendButton.UseVisualStyleBackColor = false;
+			this.removeFriendButton.Click += new System.EventHandler(this.removeFriendButton_Click);
+			// 
+			// MyFriendsComponent
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackColor = System.Drawing.Color.White;
+			this.Controls.Add(this.removeFriendButton);
+			this.Controls.Add(this.emailLabel);
+			this.Controls.Add(this.tableLayoutPanel1);
+			this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+			this.MaximumSize = new System.Drawing.Size(920, 111);
+			this.MinimumSize = new System.Drawing.Size(920, 111);
+			this.Name = "MyFriendsComponent";
+			this.Size = new System.Drawing.Size(920, 111);
+			this.tableLayoutPanel1.ResumeLayout(false);
+			((System.ComponentModel.ISupportInitialize)(this.userPicture)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
 
         }
 
