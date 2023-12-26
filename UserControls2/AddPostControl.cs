e2e10f65f730dd64ec7f942a709c400f18e6160b
Leeevai <110747443+Leeevai@users.noreply.github.com>
2023-12-26 16:10:25 +0200

    Revert "added add post"

--- UserControls2/AddPostControl.cs
+++ UserControls2/AddPostControl.cs
@@ -9,17 +9,9 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace OnlyFriends.UserControls2 {
-    
     public partial class AddPostControl : UserControl {
-        private User user;
         public AddPostControl() {
             InitializeComponent();
-           user = User.Instance;
-        }
-
-        private void postButton_Click(object sender, EventArgs e) {
-            user.addPost(postTitleInput.Text, postContentInput.Text, postTagsInput.Text);
-           
         }
     }
 }
