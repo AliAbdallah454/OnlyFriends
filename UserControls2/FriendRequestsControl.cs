ff82f3af7f3fc0c950731581f0338987a8d9dde0
AliAbdallah454 <alikhaledabdallah454@gmail.com>
2023-12-27 17:09:09 +0200

    Implemented Search Function

--- UserControls2/FriendRequestsControl.cs
+++ UserControls2/FriendRequestsControl.cs
@@ -1,78 +1,69 @@
-﻿using System;
+﻿using OnlyFriends.Components;
+using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
-using OnlyFriends.Components;
 
 namespace OnlyFriends.UserControls2 {
-    public partial class FriendRequestsControl : UserControl {
-        private SuggestedFriends suggestedFriends;
-        private FindFriend findFriend;
+	public partial class FriendRequestsControl : UserControl {
+		private SuggestedFriends suggestedFriends;
+		private FindFriend findFriend;
 
-        public FriendRequestsControl() {
-            InitializeComponent();
-            populate();
+		public FriendRequestsControl() {
+			InitializeComponent();
+			populate();
 
-            suggestedFriends = new SuggestedFriends();
-            findFriend = new FindFriend();
+			suggestedFriends = new SuggestedFriends();
+			findFriend = new FindFriend();
 
-            this.Controls.Add(suggestedFriends);
-            this.Controls.Add(findFriend);
+			this.Controls.Add(suggestedFriends);
+			this.Controls.Add(findFriend);
 
-            contentFlowLayoutPanel.SendToBack();
-        }
+			contentFlowLayoutPanel.SendToBack();
+		}
 
-        private void findFriend_Click(object sender, EventArgs e) {
-            suggestedFriends.Visible = false;
-            findFriend.Visible = !findFriend.Visible;
+		private void findFriend_Click(object sender, EventArgs e) {
+			suggestedFriends.Visible = false;
+			findFriend.Visible = !findFriend.Visible;
 
-            findFriend.Location = new Point(contentFlowLayoutPanel.Left, contentFlowLayoutPanel.Top);
-        }
+			findFriend.Location = new Point(contentFlowLayoutPanel.Left, contentFlowLayoutPanel.Top);
+		}
 
-        private void suggestedFriendButton_Click(object sender, EventArgs e) {
-            findFriend.Visible = false;
-            suggestedFriends.Visible = !suggestedFriends.Visible;
-
-
-            suggestedFriends.Location = new Point(contentFlowLayoutPanel.Left, contentFlowLayoutPanel.Top);
+		private void suggestedFriendButton_Click(object sender, EventArgs e) {
+			findFriend.Visible = false;
+			suggestedFriends.Visible = !suggestedFriends.Visible;
 
 
-        }
-
-        
+			suggestedFriends.Location = new Point(contentFlowLayoutPanel.Left, contentFlowLayoutPanel.Top);
 
 
+		}
 
-        public void populate() {
-            try {
-                User user = User.Instance;
-                List<User> friendRequests = user.getFriendRequests();
-                FriendRequestComponent[] friendRequestComponent = new FriendRequestComponent[friendRequests.Count];
+		public void populate() {
+			try {
+				User user = User.Instance;
+				List<User> friendRequests = user.getFriendRequests();
+				FriendRequestComponent[] friendRequestComponent = new FriendRequestComponent[friendRequests.Count];
 
-                for (int i = 0; i < friendRequests.Count; i++) {
+				for (int i = 0; i < friendRequests.Count; i++) {
 
-                    friendRequestComponent[i] = new FriendRequestComponent();
+					friendRequestComponent[i] = new FriendRequestComponent();
 
-                    User friend = HelperFunctions.translateUserIdToUserInfo(friendRequests[i].UserId);
+					User friend = HelperFunctions.translateUserIdToUserInfo(friendRequests[i].UserId);
 
-                    friendRequestComponent[i].FriendId = friend.UserId;
-                    friendRequestComponent[i].UserName = friend.UserName;
-                    friendRequestComponent[i].Email = friend.Email;
+					friendRequestComponent[i].FriendId = friend.UserId;
+					friendRequestComponent[i].UserName = friend.UserName;
+					friendRequestComponent[i].Email = friend.Email;
 
-                    contentFlowLayoutPanel.Controls.Add(friendRequestComponent[i]);
+					contentFlowLayoutPanel.Controls.Add(friendRequestComponent[i]);
 
-                }
-            }
-            catch(Exception ex) {
-                MessageBox.Show(ex.Message);
-            }
-        }
+				}
+			}
+			catch (Exception ex) {
+				MessageBox.Show(ex.Message);
+			}
+		}
 
-        
-    }
+
+	}
 }
